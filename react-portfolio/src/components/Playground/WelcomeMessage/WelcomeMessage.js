e7776456e53f8f3b73d8a0da8e6a7ab316a797ac
C-137-9527 <88403531+C-137-9527@users.noreply.github.com>
2022-03-10 12:07:39 -0800

    Delete react-portfolio directory

--- react-portfolio/src/components/Playground/WelcomeMessage/WelcomeMessage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import classes from './WelcomeMessage.module.css';
-
-const WelcomeMessage = () => {
-  return <div className={classes.WelcomeMessage}>WELCOME</div>;
-};
-
-export default WelcomeMessage;
